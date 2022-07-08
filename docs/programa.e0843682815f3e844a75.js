92ba17e9eee5fd7ff27e9b7a349cdb884bb0dfd6
anattolia <anattolia@gmail.com>
2022-07-08 17:27:30 -0500

    :rocket: prueba gh pages

---
+++ docs/programa.e0843682815f3e844a75.js
@@ -0,0 +1,1 @@
+!function(){"use strict";window.navigator.onLine?console.log("..:: EnFlujo ::.. conectado!"):console.log("..:: EnFlujo ::.. sin conexión!"),window.addEventListener("offline",(function(n){console.log("..:: EnFlujo ::.. sin conexión")})),window.addEventListener("online",(function(n){console.log("..:: EnFlujo ::.. conectado")}))}();
