a588c0c6b1ae76d6fa494d153ea198d712284fab
anattolia <anattolia@gmail.com>
2022-07-08 18:07:37 -0500

    Cambiar colores

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import './scss/estilos.scss';
 
+const titulo = document.getElementById('titulo');
+const forma = document.getElementById('forma');
+
 const online = window.navigator.onLine;
 if (online) {
   // Is connected to internet
   console.log('..:: EnFlujo ::.. conectado!');
+  titulo.innerText = 'Hola, hay conexión';
+  forma.style.borderRadius = '0%';
 } else {
   // Not connected to internet
+  titulo.innerText = 'Hola, no hay conexión';
+  forma.style.borderRadius = '50%';
   console.log('..:: EnFlujo ::.. sin conexión!');
 }
 
 window.addEventListener('offline', function (e) {
   // Network disconnected
+  titulo.innerText = 'Ya no hay conexión :)';
+  forma.style.borderRadius = '50%';
   console.log('..:: EnFlujo ::.. sin conexión');
 });
 
 window.addEventListener('online', function (e) {
   // Network connected
+  titulo.innerText = 'Ahora hay conexión :)';
+  forma.style.borderRadius = '0%';
   console.log('..:: EnFlujo ::.. conectado');
 });
