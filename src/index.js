1a569bc5923be1245a23e0ef6b1e00f8f57362d0
anattolia <anattolia@gmail.com>
2022-07-08 17:24:37 -0500

    :star: primer commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import './scss/estilos.scss';
-console.log('..:: EnFlujo ::..');
+
+const online = window.navigator.onLine;
+if (online) {
+  // Is connected to internet
+  console.log('..:: EnFlujo ::.. conectado!');
+} else {
+  // Not connected to internet
+  console.log('..:: EnFlujo ::.. sin conexión!');
+}
+
+window.addEventListener('offline', function (e) {
+  // Network disconnected
+  console.log('..:: EnFlujo ::.. sin conexión');
+});
+
+window.addEventListener('online', function (e) {
+  // Network connected
+  console.log('..:: EnFlujo ::.. conectado');
+});
